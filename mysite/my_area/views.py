8697c811505ec8bbc03132737137269298f41e82
saenishide <nishide.even@gmail.com>
2024-11-12 18:12:32 +0900

    新規と編集のhtmlを共通化させる

--- mysite/my_area/views.py
+++ mysite/my_area/views.py
@@ -1,89 +1,84 @@
 from django.shortcuts import render, get_object_or_404
-from my_area.service.postService import PostService
-from my_area.form.postForm import PostForm
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from my_area.service.postService import PostService
+from my_area.form.postForm import PostForm
 
-"""
-表示
-"""
-# topページを表示
+###############
+# 表示
+###############
 def display_top(request):
-    postList = PostService().getAll()
-    return render(request, 'top.html', {'postList': postList})
+    post_list = PostService().getAll()
+    return render(request, 'top.html', {'postList': post_list})
 
-# 新規作成ページを表示
 def display_create(request):
-    # form を生成
-    post = PostForm.newInstance()
-    form = PostForm(instance=post)
-    return render(request, 'create.html', {'form': form})
+    form = PostForm.newInstance()
+    return render(request, 'edit.html', {'post': form, 'mode': 'create'})
 
-# 詳細ページを表示
 def display_detail(request, post_id):
     post = PostService.getById(post_id=post_id)
     return render(request, 'detail.html', {'post': post})
 
-# 編集ページを表示
 def display_edit(request, post_id):
     post = PostService.getById(post_id)
-    return render(request, 'edit.html', {'post': post})
+    return render(request, 'edit.html', {'post': post, 'mode': 'edit'})
 
-"""
-確認表示
-"""
-# 削除確認ページを表示
+###############
+# 確認
+###############
 def confirm_delete(request, post_id):
     post = PostService.getById(post_id=post_id)
-    return render(request, 'confirm_delete.html'
-                  , {'post': {
-                      'id': post_id,
-                      'title': post.title, 
-                      'todayWord': post.todayWord,
-                      'imgUrl': post.imgUrl,
-                    }})
+    return render(request, 'confirm_delete.html', {
+        'post': {
+            'id': post_id,
+            'title': post.title,
+            'todayWord': post.todayWord,
+            'imgUrl': post.imgUrl,
+        },
+        'mode': 'delete'
+    })
 
-# 編集確認ページを表示
 def confirm_edit(request, post_id):
     post = PostService.getById(post_id)
     form = PostForm(request.POST, instance=post)
-    return render(request, 'confirm_edit.html'
-                  , {'post': {
-                      'id': post_id,
-                      'title': form.data['title'], 
-                      'todayWord': form.data['todayWord'],
-                      'imgUrl': form.data['imgUrl'],
-                      'imgUrlChange': form.data['imgUrlChange'],
-                    }})
+    return render(request, 'confirm_edit.html', {
+        'post': {
+            'id': post_id,
+            'title': form.data['title'],
+            'todayWord': form.data['todayWord'],
+            'imgUrl': form.data['imgUrl'],
+            'imgUrlChange': form.data['imgUrlChange'],
+        }
+        , 'mode': 'edit'
+    })
 
-# 新規作成確認ページを表示
 def confirm_create(request):
-    post = PostForm.newInstance()
-    form = PostForm(request.POST, instance=post)
-    return render(request
-                  , 'confirm_create.html'
-                  , {'post': {
-                      'title': form.data['title'], 
-                      'todayWord': form.data['todayWord'],
-                      'imgUrl': form.data['imgUrl'],
-                    }})
+    form = PostForm(request.POST, instance=PostForm.newInstance())
+    return render(request, 'confirm_edit.html', {
+        'post': {
+            'id': '',
+            'title': form.data['title'],
+            'todayWord': form.data['todayWord'],
+            'imgUrl': form.data['imgUrl'],
+            'imgUrlChange': form.data['imgUrlChange'],
+        }
+        , 'mode': 'create'
+    })
 
-
-"""
-操作
-"""
-# 新規作成
+###############
+# 実行
+###############
 def create_post(request):
-    post = PostForm.newInstance()
-    form = PostForm(request.POST, instance=post)
+    form = PostForm(request.POST, instance=PostForm.newInstance())
     if form.is_valid():
+        if form.data['imgUrlChange'] == 'true':
+            PostService.moveImage(request.POST['imgUrl'])
         post = form.save(commit=False)
         post.save()
-    return render(request, 'comp_add.html', {'post': post})
+        return render(request, 'comp_edit.html', {'post': post, 'mode': 'create'})
+    return render(request, 'edit.html', {'post': post, 'mode': 'create'})
 
-# 編集
 def edit_post(request, post_id):
-    # pkでデータ取得
     post = PostService.getById(post_id)
     form = PostForm(request.POST, instance=post)
     if form.is_valid():
@@ -91,10 +86,9 @@ def edit_post(request, post_id):
             PostService.moveImage(request.POST['imgUrl'])
         post = form.save(commit=False)
         post.save()
+        return render(request, 'comp_edit.html', {'post': post, 'mode': 'edit'})
+    return render(request, 'edit.html', {'post': form, 'mode': 'edit'})
 
-    return render(request, 'comp_edit.html', {'post': post})
-
-# 削除
 def delete_post(request, post_id):
     post = PostService.getById(post_id)
     form = PostForm(request.POST, instance=post)
@@ -102,16 +96,15 @@ def delete_post(request, post_id):
         post = form.save(commit=False)
         post.deleteFlag = True
         post.save()
+        return render(request, 'comp_edit.html', {'post': post, 'mode': 'delete'})
+    return render(request, 'confirm_delete.html', {'post': post, 'mode': 'delete'})
 
-    return render(request, 'comp_delete.html', {'post': post})
-
-# 画像アップロード
 @csrf_exempt
 def imgupload(request):
     image = request.FILES.get('image')
-    # ファイルをアップロード
-    if image is not None:
+    if image:
         extension = image.name.split('.')[-1]
         image.name = PostService.getHashName(image.name, extension)
         PostService.uploadImage(image)
-    return JsonResponse({"file_name": image.name})
+        return JsonResponse({"file_name": image.name})
+    return JsonResponse({"error": "No image uploaded"}, status=400)
