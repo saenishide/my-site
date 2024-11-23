29d25a0d7281fd209cf9ce610f7ca18f92b7a0fb
saenishide <nishide.even@gmail.com>
2024-11-23 09:59:00 +0900

    選択ゲームのUIとロジックを追加

--- mysite/game_area/static/js/gametop.js
+++ mysite/game_area/static/js/gametop.js
@@ -1,34 +1,58 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const zeroButton = document.getElementById('zero');
     const oneButton = document.getElementById('one');
-    zeroButton.addEventListener('click', function() {
-        checkSelect(0);
+    let correctPoint = 0;
+    zeroButton.addEventListener('click', function () {
+        settingCorrectPoint(0, correctPoint).then((result) => {
+            correctPoint = result;
+        });
     });
-    oneButton.addEventListener('click', function() {
-        checkSelect(1);
+    oneButton.addEventListener('click', function () {
+        settingCorrectPoint(1, correctPoint).then((result) => {
+            correctPoint = result;
+        });
     });
 });
 
-function checkSelect(num) {
-    const formData = new FormData();
+async function settingCorrectPoint(selectNum, nowPoint) {
     const resultArea = document.getElementById('result');
+    const comment = document.getElementById('comment');
+    let result = await checkSelect(selectNum);
+    if (result) {
+        nowPoint += 1;
+        comment.innerHTML = '正解';
+    } else {
+        nowPoint = 0;
+        comment.innerHTML = '不正解';
+    }
+    resultArea.innerHTML = `連続正解数: ${nowPoint}`;
+
+    return new Promise((resolve) => {
+        resolve(nowPoint);
+    });
+}
+
+async function checkSelect(num) {
+    const formData = new FormData();
+    let result = false;
+
     formData.append('select', num);
-    fetch('/game_area/select_zero_or_one/', {
+    await fetch('/game_area/select_zero_or_one/', {
         method: 'POST',
         body: formData,
         headers: {
             'X-CSRFToken': getCookie('csrftoken') // CSRFトークンを含める
         }
-    }).then(response => response.json())
-    .then(data => {
-            if (data['result'] === 1) {
-                resultArea.innerHTML = 'You selected success.';
-            } else {
-                resultArea.innerHTML = 'You selected fail.';
-            }
     })
-    .catch((error) => {
-        console.error('Error:', error);
+        .then(response => response.json())
+        .then(data => {
+            result = data['result'] === 1;
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    return new Promise((resolve) => {
+        resolve(result);
     });
 }
 
